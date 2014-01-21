0dfbecdabcb02108b1a9b23f6b98954478802685
VicisSilentium <kyler_jensen@hotmail.com>
2014-01-21 07:45:06 -0700

    upload

---
+++ src/Simple/Controller/StupidRunner.java
@@ -0,0 +1,15 @@
+package Simple.Controller;
+
+public class StupidRunner
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
